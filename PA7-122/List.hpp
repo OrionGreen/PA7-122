701159ad7b4f7a9a780c9711ac744fa157911d7e
OrionGreen <90157368+OrionGreen@users.noreply.github.com>
2021-11-06 16:40:59 -0700

    Finished with the Data and Stack Classes, I might need a couple operating functions within the Data Class but I think for now it's good. Finished some list stuff and the insert function is complete.

--- PA7-122/List.hpp
+++ PA7-122/List.hpp
@@ -21,13 +21,46 @@ template<class T>
 class List
 {
 public:
+	List();
+	~List();
+	Node<T>& operator= (const List<T>& rhs);
 
-	
+	//setters
+	void setmpHead(Node<T>* const newPtr);
 
+	//getters
+	Node<T>* getmpHead() const;
 
-
+	//member functions
+	bool insert(T const newData);
 
 private:
 	Node<T>* mpHead;
-	Node<T>* mpTail;
+
 };
+
+template<class T>
+void List<T>::setmpHead(Node<T>* const newPtr) {
+	this->mpHead = newPtr;
+}
+
+template<class T>
+Node<T>* List<T>::getmpHead() const {
+	return this->mpHead;
+}
+
+template<class T>
+bool List<T>::insert(T const newData) {
+	Node<T>* pMem = new Node<T>(newData);
+	bool success = false;
+
+	if (pMem != nullptr) {
+		if (this->mpHead = nullptr) {
+			this->mpHead = pMem;
+		}
+		else {
+			pMem->setNext(mpHead);
+			this->mpHead = pMem;
+		}
+	}
+}
--- PA7-122/Node.hpp
+++ PA7-122/Node.hpp
@@ -20,14 +20,60 @@ template<class T>
 class Node
 {
 public:
+	Node(T newData);
+	~Node();
+	Node<T>& operator= (const Node<T>& rhs);
 
 	//setters
+	void setData(T const newData);
+	void setNext(Node<T>* const newNode);
 
-
-
+	//getters
+	T getData() const;
+	Node* getpNext() const;
 
 private:
 	T mData;
 	Node* mpNext;
 
 };
+
+template<class T>
+Node<T>::Node(T newData) {
+	this->mData = newData;
+}
+
+template<class T>
+Node<T>::~Node() {
+
+}
+
+template<class T>
+Node<T>& Node<T>::operator= (const Node<T>& rhs) {
+	this->mData = rhs.getData();
+	this->mpNext = rhs.mpNext();
+
+	return *this;
+}
+
+//setters
+template<class T>
+void Node<T>::setData(T const newData) {
+	this->mData = newData;
+}
+
+template<class T>
+void Node<T>::setNext(Node<T>* const newNode) {
+	this->mpNext = newNode;
+}
+
+//getters
+template<class T>
+T Node<T>::getData() const {
+	return this->mData;
+}
+
+template<class T>
+Node<T>* Node<T>::getpNext() const {
+	return this->mpNext;
+}
